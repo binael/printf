39e44cdef1c4a4de3fdd176947bf1539a8a0e575
binael <nbinael@yahoo.com>
2022-04-18 15:41:47 -0700

    skeleton for printf function

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - this is the main part of the prinf fuction
+ * @format: array to print and analyse the type
+ *
+ * Return: value
+ */
+
+int _printf(const char *format, ...)
+{
+}
