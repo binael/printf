908ff14ddffd9373ac6d04440590c6b726c7e441
binael <nbinael@yahoo.com>
2022-04-19 07:47:41 -0700

    updating the main.h

--- main.h
+++ main.h
@@ -22,5 +22,15 @@ typedef struct op
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_c(va_list ar_list);
+int (*get_func(char s))(va_list ar_list);
+int print_s(va_list ar_list);
+int print_i(va_list num_list);
+int print_d(va_list ar_list);
+int print_b(va_list binary_list);
+int print_u(va_list ar_list);
+int print_o(va_list ar_list);
+int print_x(va_list ar_list);
+int print_X(va_list ar_list);
 
 #endif
