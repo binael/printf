38250e2161a7ecfc0ea9bc25277d6b3ba4011646
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-04-19 15:06:19 +0100

    printf functions

---
+++ print_i.c
@@ -0,0 +1,11 @@
+#include <main.h>
+
+/**
+* print_i - Print the integers (%i)
+* @ar_list: Store list numbers
+* Return: Number print
+*/
+int print_i(va_list ar_list)
+{
+	return (print_d(ar_list));
+}
