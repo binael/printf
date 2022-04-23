67a8035358feaabf259fce8be964400a637ac9b2
binael <nbinael@yahoo.com>
2022-04-22 21:40:08 -0700

    adding string reversal

---
+++ print_r.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_r - reverses string
+* @ar_list: Store the a list of characters
+* Return: num of parameters printed
+*/
+int print_r(va_list ar_list)
+{
+	int i, j, count = 0;
+	char *str;
+
+	str = va_arg(ar_list, char *);
+	if (str == NULL)
+		str = "(null)";
+
+	j = strlen(str);
+
+	for (i = j - 1; i >= 0; i--)
+		count += _putchar(str[i]);
+
+	return (count);
+}
